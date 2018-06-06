f49e12c858d9fb7dd8ed72f6ef0733b019de6500
rzanzuri <refaelz1@walla.co.il>
2018-06-06 20:45:44 +0300

    change the image

--- views/src/app/default/default.component.ts
+++ views/src/app/default/default.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { AppService } from '../services/app.service';
+import User from '../models/User';
 
 @Component({
   selector: 'app-default',
@@ -6,13 +8,63 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./default.component.css']
 })
 export class DefaultComponent implements OnInit {
-
-  constructor() { }
-  
+  userName = "";
+  password = "";
+  confirmPassword = "";
+  firstName = "";
+  lastName = "";
+  email = "";
+  gender = "";
+  role = "Manager";
+  active = "";
+  admin = "";
+  check = true;
   hide=false;
+
+  // navHeader: NavHeader[] = [];
+  constructor(private appService: AppService) { }
+  
   ngOnInit() {
   }
+  login() {
+    this.appService.login(new User(this.userName, this.password))
+      .subscribe(res => {
+        if (res.status == "OK") {
+          window.location.reload();
+          this.login();
+        }
+        else
+          alert(res.message);
+        // this.addItem.emit({
+        //   navheader: res,
+        // });
+      })
+  }
 
+  signup() {
+    if (this.password == this.confirmPassword) {
+      this.appService.signup(new User(this.userName, this.password,
+        this.firstName, this.lastName, this.email, this.gender, this.role))
+        .subscribe(res => {
+          alert(res.message);
+          if (res.status == "OK")
+            this.login();
+        })
+    }
+    else {
+      alert("The passwords are differents");
+      this.password = "";
+      this.confirmPassword = "";
+    }
+  }
+
+  onSelectionChange() {
+    this.check = !this.check;
+    if (!this.check)
+      this.gender = "Female";
+    else
+      this.gender = "Male";
+  }
   switch()
   {
     this.hide=!this.hide;
