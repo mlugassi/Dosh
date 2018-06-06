7593a469234a89ae763757a623a1e9f4d2e0fd75
mlugassi <michael19191@gmail.com>
2018-06-06 23:10:07 +0300

    jquery

--- views/src/app/users/users.component.ts
+++ views/src/app/users/users.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import User from '../models/User'
 import { AppService } from '../services/app.service';
+import * as $ from 'jquery';
 
 @Component({
   selector: 'app-users',
@@ -10,7 +11,7 @@ import { AppService } from '../services/app.service';
 export class UsersComponent implements OnInit {
 
   users: User[];
-  currentUser:String;
+  currentUser: String;
   private appService: AppService;
 
   constructor(appService: AppService) {
@@ -33,10 +34,30 @@ export class UsersComponent implements OnInit {
   deleteUser(userName: String) {
   }
 
-  editPassword(){
-
+  editPassword() {
+    if ($("#checkbox-pass-editUser").prop('checked')) {
+      $("#lopass-editUser").show();
+      $("#opass-editUser").show();
+      $("#lpass-editUser").show();
+      $("#pass-editUser").show();
+      $("#lvpass-editUser").show();
+      $("#vpass-editUser").show();
+      $("#opass-editUser").prop('required', true);
+      $("#pass-editUser").prop('required', true);
+      $("#vpass-editUser").prop('required', true);
+    } else {
+      $("#lopass-editUser").hide();
+      $("#opass-editUser").hide();
+      $("#lpass-editUser").hide();
+      $("#pass-editUser").hide();
+      $("#lvpass-editUser").hide();
+      $("#vpass-editUser").hide();
+      $("#opass-editUser").prop('required', false);
+      $("#pass-editUser").prop('required', false);
+      $("#vpass-editUser").prop('required', false);
+    }
   }
-  updateUser(){
+  updateUser() {
 
   }
 }
