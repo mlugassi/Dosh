a3af8d2bf4dcf94ef61ee459d8269a0babbf4cf5
mlugassi <michael19191@gmail.com>
2018-06-17 00:55:08 +0300

    blogs #2

--- views/src/app/main-page/blogs/blogs.component.ts
+++ views/src/app/main-page/blogs/blogs.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-import * as $ from 'jquery';
+import { AppService } from '../../services/app.service';
+import Blog from '../../models/Blog';
 
 @Component({
   selector: 'app-blogs',
@@ -8,22 +9,84 @@ import * as $ from 'jquery';
 })
 export class BlogsComponent implements OnInit {
 
-  constructor() { }
+  myBlogs: Blog[];
+  allBlogs: Blog[];
+
+  constructor(private appService: AppService) { }
 
   ngOnInit() {
-    var acc = document.getElementsByClassName("accordion");
-    var i;
-    
-    for (i = 0; i < acc.length; i++) {
-      acc[i].addEventListener("click", function() {
-        this.classList.toggle("active");
-        var panel = this.nextElementSibling;
-        if (panel.style.maxHeight){
-          panel.style.maxHeight = null;
-        } else {
-          panel.style.maxHeight = panel.scrollHeight + "px";
-        } 
+    this.appService.get_my_blogs().subscribe(res => {
+      this.myBlogs = [];
+      res.forEach(blog => {
+        blog.created_at = this.setDateString(blog.created_at);
+        this.myBlogs.push(blog);
+      })
+    });
+
+    this.appService.get_all_blogs_but_mine().subscribe(res => {
+      this.allBlogs = [];
+      res.forEach(blog => {
+        blog.created_at = this.setDateString(blog.created_at);
+        this.allBlogs.push(blog);
       });
+    });
+  }
+
+  setDateString(date) {
+    let day = date.substr(8, 2);
+    let month = date.substr(5, 2);
+    let year = date.substr(0, 4);
+
+    switch (month) {
+      case "01": {
+        month = "JAN";
+        break;
+      }
+      case "02": {
+        month = "FEB";
+        break;
+      }
+      case "03": {
+        month = "MAR";
+        break;
+      }
+      case "04": {
+        month = "APR";
+        break;
+      }
+      case "05": {
+        month = "MAY";
+        break;
+      }
+      case "06": {
+        month = "JUN";
+        break;
+      }
+      case "07": {
+        month = "JUL";
+        break;
+      }
+      case "08": {
+        month = "AUG";
+        break;
+      }
+      case "09": {
+        month = "SEP";
+        break;
+      }
+      case "10": {
+        month = "OCT";
+        break;
+      }
+      case "11": {
+        month = "NOV";
+        break;
+      }
+      case "12": {
+        month = "DEC";
+        break;
+      }
     }
+    return day + " " + month + ", " + year;
   }
 }
